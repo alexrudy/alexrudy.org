bceca7771e7836cbc9eb3256b9b7dd68c229e468
Alex Rudy <alex.rudy@gmail.com>
2019-12-24 17:04:10 -0800

    Add tasks to run a jekyll server in a container and watch

---
+++ tasks.py
@@ -0,0 +1,44 @@
+from invoke import task, Collection
+
+import os
+import json
+import pprint
+from pathlib import Path
+
+ns = Collection()
+
+def t(*args, **kwargs):
+    """Custom task wrapper which also registers task in a namespace"""
+
+    namespace = kwargs.pop("ns", ns)
+
+    def _inner(f):
+        _task = task(*args, **kwargs)(f)
+        namespace.add_task(_task)
+        return _task
+
+    return _inner
+
+def dc(c, command, **kwargs):
+    c.run(f"docker-compose -f docker-compose.yml -f docker-compose.dev.yml {command}", **kwargs)
+
+@t(aliases=("s", "server"))
+def serve(c):
+    """Run the draft server."""
+    c.config.run.pty = True
+    dc(c, "exec nginx-alexrudy bundle exec jekyll serve --watch --drafts --host 0.0.0.0")
+
+@t()
+def up(c):
+    """Stand up the production service"""
+    dc(c, "up -d")
+
+@t()
+def down(c):
+    """Bring down the service"""
+    dc(c, "down")
+
+@t()
+def build(c):
+    """Build the Jekyll container"""
+    dc(c, "-f docker-compose.ci.yml build")
